7db872a6a43e2931e443fceb6e74579f9efac88f
Rahmir Ford <Rahmir.ford@gmail.com>
2017-05-20 21:05:05 -0400

    initial commit

---
+++ src/main/java/DriversLicense.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+/**
+ * Created by rahmirford on 5/20/17.
+ */
+public class DriversLicense {
+
+
+    public static String deserializeFromCSV(String driver){
+        return null;
+    }
+
+    public static List<DriversLicense> serializeToCSV(){
+        return null;
+    }
+
+}
---
+++ src/test/java/TestDriversLicense.java
@@ -0,0 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by rahmirford on 5/20/17.
+ */
+public class TestDriversLicense {
+
+    DriversLicense dl;
+
+    @Before
+    public void setup(){
+        dl = new DriversLicense();
+    }
+
+    @Test
+    public void testDeserializeFromCSV(){
+
+    }
+
+    @Test
+    public void testSerializeToCSV(){
+        
+    }
+
+}
